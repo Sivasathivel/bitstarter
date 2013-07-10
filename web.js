30a74c9bdd73c91262bd1099d46237afeef5c7a3
John Smith <example@stanford.edu>
2013-07-10 13:42:46 +0000

    updated Web.js

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -10,3 +11,22 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+
+// Modified by Sivasathivel K on 10 Jul 2013
+
+var fs = require('fs');
+var express = require('express');
+
+var data = fs.readFileSync("index.html","utf8");
+var app = express.createrServer(express.logger());
+app.get('/', function(request, response) {
+    response.send(data);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Linstening on Port:" + port);
+});
+
+
